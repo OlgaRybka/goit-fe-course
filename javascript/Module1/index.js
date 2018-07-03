4de478999136500e3d4b8a3a12b098052ab1a852
Olga <ytg.ukraine@gmail.com>
2018-07-03 07:54:07 +0200

    updates

--- javascript/Module1/index.js
+++ javascript/Module1/index.js
@@ -1,22 +1,25 @@
 'use strict';
 
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r';
+
 let denied = "Отменено пользователем!";
 let forbidden= "Доступ запрещен!"
 let welcome="Добро пожаловать!"
 
-const ADMIN_LOGIN = prompt ("Please enter your login", " ");
-if (ADMIN_LOGIN===null) {
+const login  = prompt ("Please enter your login", " ");
+if (login ===null) {
     alert (denied);
 }
-else if (ADMIN_LOGIN !== "admin") {
+else if (login !== ADMIN_LOGIN) {
     alert (forbidden);
 }
-else {const ADMIN_PASSWORD =prompt("Please enter your password"," ");
+else {const password =prompt("Please enter your password"," ");
 
-if (ADMIN_PASSWORD ===null){ 
+if (password ===null){ 
     alert (denied);
 }
-else if (ADMIN_PASSWORD !== "m4ngo1zh4ackz0r") { 
+else if (password !== ADMIN_PASSWORD) { 
     alert (forbidden);
 }
 else { alert (welcome);
---
+++ javascript/Module2/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const input = 0;
+const arr =[]
+
+do {
+  input = prompt('Введите число', '');
+} while(input !== null);
+
+console.log(input)
